33259698dc88689d82f518d8f99feaaf210eef46
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-12-18 12:54:39 -0300

    Seed usuário/time

---
+++ database/seeds/DatabaseSeeder.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| DatabaseSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const User = use('App/Models/User')
+
+class DatabaseSeeder {
+  async run () {
+    const user = await User.create({
+      name: 'Osvaldo Kalvaitir Filho',
+      email: 'osvaldokalvaitir@outlook.com',
+      password: '123456',
+    });
+
+    await user.teams().create({
+      name: 'Rocketseat',
+      user_id: user.id
+    });
+  }
+}
+
+module.exports = DatabaseSeeder
