b6f80f0736446ae3b6d2503855a08706519f74a4
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-12-26 20:55:43 -0300

    Permissões

--- start/routes.js
+++ start/routes.js
@@ -27,5 +27,9 @@ Route.group(() => {
     )
 
   Route.get('members', 'MemberController.index')
-  Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
+  Route.put('members/:id', 'MemberController.update').middleware(
+    'is:administrator'
+  )
+
+  Route.get('permissions', 'PermissionController.show')
 }).middleware(['auth', 'team'])
